9b78ec918c37f43d3c24bca15fc9a011f05d7780
Narciso Gomes <narcisoalmeida25@gmail.com>
2021-05-26 15:44:21 -0300

    feat: add simple cors rules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+const corsOption = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'))
 
 app.use(require('./routes'));
+
+app.use(cors(corsOption));
 
 app.listen(3000);
