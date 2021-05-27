11c731a33a62a4c038b2308c2af8d85a791fc00f
Narciso Gomes <narcisoalmeida25@gmail.com>
2021-05-27 11:02:26 -0300

    fix: set message upload success

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,7 @@ const multer =  require('multer');
 const multerConfig = require('./config/multer')
 
 routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
-    console.log(req.file);
-    return res.json({ hello: 'Hello world' });
+    return res.json({ message: 'upload success' });
 });
 
 module.exports = routes;
